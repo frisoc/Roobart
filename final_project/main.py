703b6186d9487cb89a5892b551980988c3eeabee
frisoc <frisoc@msoe.edu>
2021-11-06 16:14:04 -0500

    add return statement for detect_bombs

--- final_project/main.py
+++ final_project/main.py
@@ -25,58 +25,50 @@ color_sensor = ColorSensor(Port.S1)
 d = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=114.0) #might need to change last 2 params
 d.settings(700, 100, 100, 100)
 
-# Write your program here.
+# functions
 
-
-
-def calculate_time(distance, rpm):
+def detect_bombs(granularity, turn_distance, edge_distance, bomb_color, field_color):
     """
-    placeholder until we can properly calculate time
+    granularity: How far the robot will step forward when traveling along y in mm
+    turn_distance: How far the robot will step after turning 90 degrees in mm
+    edge_distance: distance of each edge of area in mm
+    bomb_color: color of the bombs
+    field_color: color of the field
+    return: list of points
     """
-    diameter = 17.28
-    time = (rpm * 17.28) / distance
-    time = 0
-    return time
-
-def detect_bombs(distance):
-    rpm_straight = 120
-    time_straight = calculate_time(distance, rpm)
-
-                                              
-    num_turns = distance / ROBOT_DIAMETER
-    left_motor.run_time(rpm, time, wait=False)
-    right_motor.run_time(rpm, time)
-    ev3.speaker.beep()
-    
-
-# detect_bombs(25)
-# d.straight(150)
-# print(color_sensor.color())
-# d.turn(90)
-# d.straight(150)
-# d.turn(90)
-# ev3.speaker.say("")
-
-
-total_y = 0
-total_x = 0
-granularity = 150 # How far the robot will step forward when traveling along y in mm
-turn_distance = 150 # How far the robot will step after turning 90 degrees in mm
-edge_distance = 300 # distance of each edge of area in mm
-bomb_color = Color.BLUE
-field_color = Color.BLACK
-points = []
-direction = 1
-
-while total_x <= edge_distance:
-    while total_y <= edge_distance:
-        d.straight(granularity)
-        if color_sensor.color() == Color.BLUE:
-            points.append([total_x, total_y])
-        total_y += granularity
     total_y = 0
-    d.turn(90 * direction)
-    d.straight(turn_distance)
-    d.turn(90 * direction)
-    total_x += turn_distance
-    direction = direction * -1
+    total_x = 0
+    # bomb_color = Color.BLUE
+    # field_color = Color.BLACK
+    points = []
+    direction = 1
+    while total_x <= edge_distance:
+        while total_y <= edge_distance:
+            d.straight(granularity)
+            if color_sensor.color() == bomb_color:
+                points.append([total_x, total_y])
+                ev3.speaker.say("Bomb Detected")
+                print(total_x, total_y)
+                print(points)
+            total_y += granularity
+        total_y = 0
+        d.turn(90 * direction)
+        d.straight(turn_distance)
+        d.turn(90 * direction)
+        total_x += turn_distance
+        direction = direction * -1
+    ev3.speaker.say("Scan complete")
+    return points
+
+# write your program here
+granularity = 150
+turn_distance = 150 
+edge_distance = 300 
+
+points = detect_bombs(granularity, turn_distance, edge_distance, Color.BLACK, Color.BROWN)
+print(points)
+# print("color 1: ", color_sensor.color())
+# ev3.speaker.say("Detecting Color")
+# d.straight(150)
+# print('color 2: ', color_sensor.color())
+# ev3.speaker.say("Detecting Color")
