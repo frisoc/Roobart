fdca92f61948a7d7b39324ea792179470cd4ee17
botzocc <58751555+botzocc@users.noreply.github.com>
2021-11-06 19:47:26 -0500

    starting to create a mine class

--- graph-traversal/graph_testing.py
+++ graph-traversal/graph_testing.py
@@ -1,3 +1,5 @@
+# https://pythoninwonderland.wordpress.com/2017/03/18/how-to-implement-breadth-first-search-in-python/
+
 def bfs_connected_component(graph, start):
     explored = []
     queue = [start]
@@ -13,6 +15,55 @@ def bfs_connected_component(graph, start):
     return explored
 
 
-nodes = {1: [50, 30], 2: [100, 155], 3: [200, 155],
-         4: [305, 110], 5: [110, 280], 6: [340, 275],
-         7: [90, 430], 8: [230, 380], 9: [335, 475]}
+def bfs_shortest_path(graph, start, goal):
+    explored = []
+    queue = [[start]]
+
+    if start == goal:
+        return "That was easy! Start = goal"
+
+    while queue:
+        path = queue.pop(0)
+        node = path[-1]
+        if node not in explored:
+            neighbours = graph[node]
+            for neighbour in neighbours:
+                new_path = list(path)
+                new_path.append(neighbour)
+                queue.append(new_path)
+                if neighbour == goal:
+                    return new_path
+
+            explored.append(node)
+
+    return []
+
+
+# start = [0, 0]
+# nodes = {1: [50, 30], 2: [100, 155], 3: [200, 155],
+#          4: [305, 110], 5: [110, 280], 6: [340, 275],
+#          7: [90, 430], 8: [230, 380], 9: [335, 475]}
+# print(bfs_connected_component(list(nodes.values()), start))
+
+graph = {'A': ['B', 'C', 'E'],
+         'B': ['A', 'D', 'E'],
+         'C': ['A', 'F', 'G'],
+         'D': ['B'],
+         'E': ['A', 'B', 'D'],
+         'F': ['C'],
+         'G': ['C']}
+print(bfs_connected_component(graph, "A"))
+print(bfs_shortest_path(graph, "A", "G"))
+
+mine_graph = {"A": ["B"],
+              "B": ["A", "C", "D"],
+              "C": ["B", "D", "G", "H"],
+              "D": ["B", "C", "E", "F"],
+              "E": ["D"],
+              "F": ["F"],
+              "G": ["C", "I"],
+              "H": ["C"],
+              "I": ["G"]
+              }
+print(bfs_connected_component(mine_graph, "A"))
+print(bfs_shortest_path(mine_graph, "A", "G"))
